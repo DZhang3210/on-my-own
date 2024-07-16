934ad6e8331d9d3c41a4a025982d8c4e4fcc89b4
DZhang3210 <53838139+DZhang3210@users.noreply.github.com>
2024-07-15 20:37:11 -0400

    extra

--- app/profile/page.js
+++ app/profile/page.js
@@ -3,10 +3,14 @@ import { authOptions } from '@/lib/authOptions'
 import { Appointment } from '@/models/Appointment'
 import mongoose from 'mongoose'
 import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 
 const Profile = async () => {
     const session = await getServerSession(authOptions)
-    if (!session) {console.log("Couldn't find Session")}
+    if (!session) {
+        console.log("Couldn't find Session")
+        redirect('/')
+    }
     const currentDate = new Date();
     await mongoose.connect(process.env.MONGODB_URI)
     const appointments = await Appointment.find({
