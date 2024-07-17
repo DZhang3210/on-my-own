b8b6b3572668f8a12bbc7d598106c2a6c931e732
DZhang3210 <53838139+DZhang3210@users.noreply.github.com>
2024-07-17 11:08:17 -0400

    new commit

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,4 +1,4 @@
-import ProfilePage from '@/components/profile/ProfilePage'
+import ProfilePage from '@/components/profile/profilePage'
 import { authOptions } from '@/lib/authOptions'
 import { Appointment } from '@/models/Appointment'
 import mongoose from 'mongoose'
